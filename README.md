e5f7c5dff2530ca3a976519ec77be86393346632
renatocortez <recortez@gmail.com>
2022-04-06 17:39:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# grocery-bud
+ grocery-bud
---
+++ js/app.js
@@ -0,0 +1,20 @@
+// ****** SELECT ITEMS **********
+
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector("submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
